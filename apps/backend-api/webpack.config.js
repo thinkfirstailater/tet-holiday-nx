0034cbdacc010f9a88282190a0f23520d8e2a384
thinkfirstailater <think.first.ai.later@gmail.com>
2026-02-12 22:26:36 +0700

    Skiplib check

--- apps/backend-api/webpack.config.js
+++ apps/backend-api/webpack.config.js
@@ -2,6 +2,7 @@ const { composePlugins, withNx } = require('@nx/webpack');
 
 module.exports = composePlugins(withNx({
   target: 'node',
+  skipTypeCheck: true, // Disable type checking to save memory during build
 }), (config) => {
   // Update the webpack config as needed here.
   // e.g. config.plugins.push(new MyPlugin())
